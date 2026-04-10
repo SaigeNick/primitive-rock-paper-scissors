30e2cf14f7dce1e576b689b358c662fbe1cd2441
SaigeNick <danya1102003@gmail.com>
2026-04-10 12:48:23 +0300

    Editted game logic to work with buttons
    Like said, editted game logic, which is now around buttons for the human choice. No styling as of now, and no end conditions.

--- game_logic.js
+++ game_logic.js
@@ -34,50 +34,82 @@ function getHumanChoice(){
     return choice
 }
 
+const btnRock = document.querySelector(".btnRock");
+btnRock.addEventListener("click", () => {
+    playRound("rock")
+});
 
-function playRound(humanChoice, computerChoice){
+const btnPaper = document.querySelector(".btnPaper");
+btnPaper.addEventListener("click", () => {
+    playRound("paper")
+});
+
+const btnScissors = document.querySelector(".btnScissors");
+btnScissors.addEventListener("click", () => {
+    playRound("scissors")
+});
+
+const scoreContainer = document.querySelector(".scoreContainer");
+const result = document.createElement("p");
+result.classList.add("result")
+const score = document.createElement("p");
+score.classList.add("score")
+
+
+
+function playRound(humanChoice){
+    let computerChoice = getComputerChoice()
     if (humanChoice === computerChoice){
+        result.textContent = "It's tie! You and computer both chose the same thing."
         console.log("It's tie! You and computer both chose the same thing.")
     } else {
         switch (humanChoice){
             case "rock":
                 if (computerChoice === "paper"){
+                    result.textContent = "You lose! Paper beats rock."
                     console.log("You lose! Paper beats rock")
                     computerScore++
                 } else {
+                    result.textContent = "You win! Rock beats scissors."
                     console.log("You win! Rock beats scissors")
                     humanScore++
                 }
                 break
             case "paper":
                 if (computerChoice === "scissors"){
+                    result.textContent = "You lose! Scissors beat paper."
                     console.log("You lose! Scissors beat paper")
                     computerScore++
                 } else {
+                    result.textContent = "You win! Paper beats rock."
                     console.log("You win! Paper beats rock")
                     humanScore++
                 }
                 break
             case "scissors":
                 if (computerChoice === "rock"){
+                    result.textContent = "You lose! Rock beats scissors."
                     console.log("You lose! Rock beats scissors")
                     computerScore++
                 } else {
+                    result.textContent = "You win! Scissors beat paper."
                     console.log("You win! Scissors beat paper")
                     humanScore++
                 }
                 break
         }
+        scoreContainer.appendChild(result)
+        let scoreText = 
+        score.textContent = "Your score - " + humanScore + " Computer score - " + computerScore
+        scoreContainer.appendChild(score)
     }
 }
 
 
 function playGame(){
-    for(let i = 0; i < 5; i++){
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection);
     if (humanScore === computerScore){
         console.log("It's tie, nobody wins. Your score is " + humanScore + " Computer's score is " + computerScore)
     } else if (humanScore > computerScore){
